8088f3c33661da2d140e865c5dcb912e63ee4749
linhtiendev <lelinhtien102@gmail.com>
2024-12-20 20:54:21 +0700

    feat/implement-navigation-for-modules-and-actions

--- config.php
+++ config.php
@@ -1,1 +1,5 @@
 <!-- các hằng của pj -->
+<?php
+
+const _MODULE = 'home'; // điều hướng mặc định là trang home
+const _ACTION = 'dashboard'; // mặc định trang dashboard nếu action rỗng
--- index.php
+++ index.php
@@ -1,1 +1,39 @@
 <!-- file chính -->
+<?php
+// khởi tạo session
+session_start();
+
+// gọi file config
+require_once('config.php');
+
+$module = _MODULE;
+$action = _ACTION;
+
+// Hàm điều hướng modules
+// dk lấy module
+// check rỗng
+if (!empty($_GET['module'])) {
+    // check chuỗi
+    if (is_string($_GET['module'])) {
+        $module = trim($_GET['module']);
+    }
+}
+
+// dk lấy action trong module
+if (!empty($_GET['action'])) {
+    if (is_string($_GET['action'])) {
+        $action = trim($_GET['action']);
+    }
+}
+
+// có được module và action rồi thì ghép lại thành path để điều hướng
+$path = 'modules/' . $module . '/' . $action . '.php';
+
+// kiểm tra file có tồn tại
+if (file_exists($path)) {
+    // dùng require để thực hiện điều hướng
+    require_once($path);
+} else {
+    // sai path sẽ điều hướng sang error
+    require_once 'modules/error/404.php';
+}
